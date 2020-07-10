ac2f34f1915baa497332599ef304884b32bb86d3
nursyahirahamirahariffin <67114435+nursyahirahamirahariffin@users.noreply.github.com>
2020-07-11 03:33:09 +0800

    add email

--- suppliersignup.php
+++ suppliersignup.php
@@ -49,7 +49,118 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 
 				if(mysqli_query($conn, $insert))
 				{
-					
+use PHPMailer\PHPMailer\PHPMailer;
+									use PHPMailer\PHPMailer\Exception;
+									
+									/* Exception class. */
+									require 'PHPMailer-master\src\Exception.php';
+									
+									/* The main PHPMailer class. */
+									require 'PHPMailer-master\src\PHPMailer.php';
+									
+									/* SMTP class, needed if you want to use SMTP. */
+									require 'PHPMailer-master\src\SMTP.php';
+									
+									/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+									$mail = new PHPMailer(TRUE);
+
+									/**Value is staffid coming from supplierloginaction.php**/
+									$empid = $_SESSION['login_employee'];						
+
+									$sql1 = "SELECT * FROM `employee` e, `supplier` s
+											WHERE e.empid=s.empid";
+									
+									$result1 = $conn->query($sql1);
+
+									$val = array();
+
+									/* Retrieve data from db. Cannot use fetch_assoc() */
+									if($result1->num_rows > 0){
+										while($row = mysqli_fetch_array($result1)){
+											$val[] = $row;
+										}						
+									}
+									else {
+										$val = [];
+									}
+
+									/* Get supplieremail and empfname from $val */
+									foreach($val as $row){
+										$email = $row["supplieremail"];
+										$name = $row["empfname"];
+									}
+
+									try {
+										//Server settings
+										//$mail->SMTPDebug = 2;                                       // Enable verbose debug output
+										$mail->isSMTP();                                            // Set mailer to use SMTP
+										$mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+										$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+										$mail->Username   = 'nursyahirahamirahariffin@gmail.com';                     // SMTP username
+										$mail->Password   = 'Incorrectpassword';                               // SMTP password
+										$mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
+										$mail->Port       = 587;                                    // TCP port to connect to
+									
+										//Recipients
+										$mail->setFrom('admin@example.com', $suppID);
+										$mail->addAddress($email, $name); 
+									
+										// Attachments
+										//$mail->addAttachment('/home/cpanelusername/attachment.txt');         // Add attachments
+										//$mail->addAttachment('/home/cpanelusername/image.jpg', 'new.jpg');    // Optional name
+										//$mail->addAttachment('email.html');  
+						
+										// Content
+										$mail->isHTML(true);                                  // Set email format to HTML
+										$mail->Subject = 'Order Approved';
+										$mail->Body    = file_get_contents('email.html');
+										//$mail->Body    = include('email.html');
+										$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+										//$headers .= "MIME-Version: 1.0\r\n";
+										//$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+										$mail->send();
+										echo '';
+									
+									}
+									catch (Exception $e) {
+										echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+									}
+								?>
+							</article>
+						</div>
+					</div>
+					<!-- End of row -->
+
+					<hr>
+					<!-- Second row -->
+					<div class="row">
+						<div class="col-md-4 col-sm-4">
+							<div class="panel panel-success">
+								<div class="panel-heading"> New Created Supplier Account Info</div>
+								<div class="panel-body">
+									<p>Details: </p>
+									<?php
+										echo "<p>Your account was created by: "; echo $empfname; echo "from Tomatus Station</p>";
+									?>									
+									<p>Your account has been successfully created<p>
+								</div>
+								<div class="panel-footer"> Your Satisfaction Is Our Priority </div>
+							</div>
+						</div>
+
+						<div class="col-md-4 col-sm-4">
+							<div class="panel panel-info">
+								<div class="panel-heading"> Messages Info </div>
+								<div class="panel-body">
+									<p>New record created successfully for invoice</p>
+									<p>Stock for the product has been updated</p>
+								</div>
+								<div class="panel-footer"> Your Satisfaction Is Our Priority </div>
+							</div>
+						</div>
+					</div>
+					<!-- End of Second Row -->
 				}
 				else
 				{
