08c09245a39e746893259e0963c7dcb917c84813
nasuhasri <63119811+nasuhasri@users.noreply.github.com>
2020-07-12 00:42:16 +0800

    update

--- employeelogin.php
+++ employeelogin.php
@@ -1,141 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<link rel="shortcut icon" href="images/favicon.ico" />
-</head>
+     <head>
+          <link rel="shortcut icon" href="images/favicon.ico" />
+     </head>
 
-<head>
+     <head>
+          <title>Tomatus Station</title>
 
-<title>Tomatus Station</title>
+          <!--
 
-<!--
+          Template 2099 Scenic
 
-Template 2099 Scenic
+          http://www.tooplate.com/view/2099-scenic
 
-http://www.tooplate.com/view/2099-scenic
+          -->
 
--->
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+          <meta name="description" content="">
+          <meta name="keywords" content="">
+          <meta name="author" content="">
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-<meta name="description" content="">
-<meta name="keywords" content="">
-<meta name="author" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+          <link rel="stylesheet" href="css/bootstrap.min.css">
+          <link rel="stylesheet" href="css/font-awesome.min.css">
+          <link rel="stylesheet" href="css/magnific-popup.css">
 
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/font-awesome.min.css">
-<link rel="stylesheet" href="css/magnific-popup.css">
+          <link rel="stylesheet" href="css/owl.theme.css">
+          <link rel="stylesheet" href="css/owl.carousel.css">
 
-<link rel="stylesheet" href="css/owl.theme.css">
-<link rel="stylesheet" href="css/owl.carousel.css">
-
-<!-- MAIN CSS -->
-<link rel="stylesheet" href="css/tooplate-style.css">
-
-</head>
+          <!-- MAIN CSS -->
+          <link rel="stylesheet" href="css/tooplate-style.css">
+     </head>
 
 
-<body>
-
-<!-- PRE LOADER -->
-<div class="preloader">
-     <div class="spinner">
-          <span class="sk-inner-circle"></span>
-     </div>
-</div>
-
-
-<!-- MENU -->
-<div class="navbar custom-navbar navbar-fixed-top" role="navigation">
-     <div class="container">
-
-          <!-- NAVBAR HEADER -->
-          <div class="navbar-header">
-               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon icon-bar"></span>
-                    <span class="icon icon-bar"></span>
-                    <span class="icon icon-bar"></span>
-               </button>
-               <!-- lOGO -->
-               <a href="welcomepage.php" class="navbar-brand">Tomatus Station Melaka</a>
+     <body>
+          <!-- PRE LOADER -->
+          <div class="preloader">
+               <div class="spinner">
+                    <span class="sk-inner-circle"></span>
+               </div>
           </div>
 
-          <!-- MENU LINKS -->
-          <div class="collapse navbar-collapse">
-               <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#more" class="smoothScroll">About</a></li>  
-                    <li><a href="#more" class="dropdown">Contact</a></li>
-					<li><a href="signup.php" class="smoothScroll">Register</a></li>
-               </ul>
+
+          <!-- MENU -->
+          <div class="navbar custom-navbar navbar-fixed-top" role="navigation">
+               <div class="container">
+
+                    <!-- NAVBAR HEADER -->
+                    <div class="navbar-header">
+                         <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                              <span class="icon icon-bar"></span>
+                              <span class="icon icon-bar"></span>
+                              <span class="icon icon-bar"></span>
+                         </button>
+                         <!-- lOGO -->
+                         <a href="welcomepage.php" class="navbar-brand">Tomatus Station Melaka</a>
+                    </div>
+
+                    <!-- MENU LINKS -->
+                    <div class="collapse navbar-collapse">
+                         <ul class="nav navbar-nav navbar-right">
+                              <li><a href="#more" class="smoothScroll">About</a></li>  
+                              <li><a href="#more" class="dropdown">Contact</a></li>
+                                   <li><a href="signup.php" class="smoothScroll">Register</a></li>
+                         </ul>
+                    </div>
+               </div>
+               <!-- End of div container -->
           </div>
-
-     </div>
-</div>
+          <!-- End of of div navigation -->
 
 
-<!-- HOME -->
-<section id="home" class="parallax-section">
-     <div class="overlay"></div>
-     <div class="container">
-          <div class="row">
+          <!-- HOME -->
+          <section id="home" class="parallax-section">
+               <div class="overlay"></div>
+               <div class="container">
+                    <div class="row">
+                         <div class="col-md-8 col-sm-12">
+                              <div class="home-text">
+                                   <article> 
+                                        <h1 style="text-align:left">Staff Login</h1>
+                                        <form action="employeeloginaction.php" id="form" method="POST">                         
+                                             <div class="container">
+                                                  <h4 style="color:white" for="empID"><b>Employee ID</b></h4>
+                                                  <input type="text" placeholder="Enter Employee ID" name="empID" required>
 
-               <div class="col-md-8 col-sm-12">
-                    <div class="home-text">
-						 <article> 
-							<h1 style="text-align:left">Staff Login</h1>
-							<form action="employeeloginaction.php" id="form" method="POST">
-                   
-								<div class="container">
-									
-											<h4 style="color:white" for="empID"><b>Employee ID</b></h4>
-											<input type="text" placeholder="Enter Employee ID" name="empID" required>
-
-											<h4 style="color:white" for="empPassword"><b>Password</b></h4>
-											<input type="password" placeholder="Enter Password" name="empPassword" required>
-											<br></br>
-											<br></br>
-
-									<table>
-										<tr>
-											<td>
-											<input type="submit" style="background-color:green;color:white;width:150px; height:40px;" value="Login">
-											<input type="button" onclick="history.back()" style="background-color:red;color:white;width:150px; height:40px;" value="Back">
-											</td>
-										</tr>
-									</table>
-								
-								</div>
-                          </form>
-			           </article> 
+                                                  <h4 style="color:white" for="empPassword"><b>Password</b></h4>
+                                                  <input type="password" placeholder="Enter Password" name="empPassword" required>
+                                                  <br></br>
+                                                  <br></br>
+                                                  <table>
+                                                       <tr>
+                                                            <td>
+                                                                 <input type="submit" style="background-color:green;color:white;width:150px; height:40px;" value="Login">
+                                                                 <input type="button" onclick="history.back()" style="background-color:red;color:white;width:150px; height:40px;" value="Back">
+                                                            </td>
+                                                       </tr>
+                                                  </table>
+                                             </div>
+                                        </form>
+			                    </article> 
+                              </div>
+                         </div>
                     </div>
                </div>
 
-          </div>
-     </div>
-
-     <!-- Video -->
-     <video controls autoplay loop muted>
-          <source src="videos/video.mp4" type="video/mp4">
-          Your browser does not support the video tag.
-     </video>
-</section>
+               <!-- Video -->
+               <video controls autoplay loop muted>
+                    <source src="videos/video.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+               </video>
+          </section>
 
 
-<footer>
-<?php include 'footer.php';?>
-</footer>
+          <footer>
+               <?php include 'footer.php';?>
+          </footer>
 
-<!-- SCRIPTS -->
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.parallax.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script src="js/magnific-popup-options.js"></script>
-<script src="js/modernizr.custom.js"></script>
-<script src="js/smoothscroll.js"></script>
-<script src="js/custom.js"></script>
-
-</body>
+          <!-- SCRIPTS -->
+          <script src="js/jquery.js"></script>
+          <script src="js/bootstrap.min.js"></script>
+          <script src="js/jquery.parallax.js"></script>
+          <script src="js/owl.carousel.min.js"></script>
+          <script src="js/jquery.magnific-popup.min.js"></script>
+          <script src="js/magnific-popup-options.js"></script>
+          <script src="js/modernizr.custom.js"></script>
+          <script src="js/smoothscroll.js"></script>
+          <script src="js/custom.js"></script>
+     </body>
 </html>
