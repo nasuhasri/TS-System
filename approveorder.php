c087345f2a102f17dd89863590cb4875cdca2fef
nasuhasri <63119811+nasuhasri@users.noreply.github.com>
2020-08-26 14:51:23 +0800

    update

--- approveorder.php
+++ approveorder.php
@@ -197,8 +197,8 @@
 										$mail->isSMTP();                                     // Set mailer to use SMTP
 										$mail->Host       = 'smtp.gmail.com';                // Specify main and backup SMTP servers
 										$mail->SMTPAuth   = true;                            // Enable SMTP authentication
-										$mail->Username   = 'nasuhasri00@gmail.com';         // SMTP username
-										$mail->Password   = 'Android00';                     // SMTP password
+										$mail->Username   = '';         // SMTP username
+										$mail->Password   = '';                     // SMTP password
 										$mail->SMTPSecure = 'tls';                           // Enable TLS encryption, [ICODE]ssl[/ICODE] also accepted
 										$mail->Port       = 587;                             // TCP port to connect to
 									
